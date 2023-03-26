895c2b48109fb8388105484725df4d0be419b6af
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-26 13:36:40 -0500

    hardcode #4, working

--- conditionals2_practice_problems.rb
+++ conditionals2_practice_problems.rb
@@ -49,30 +49,49 @@
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
-number_1 = 666
-number_2 = 666
+# number_1 = 666
+# number_2 = 666
 
-if number_1 < 10 && number_2 < 10
-  puts "1"
-else
-  puts "0"
-end
+# if number_1 < 10 && number_2 < 10
+#   puts "1"
+# else
+#   puts "0"
+# end
 
 
-number_1 = 6
-number_2 = -6
+# number_1 = 6
+# number_2 = -6
 
-if number_1 < 10 && number_2 < 10
-  puts "1"
-else
-  puts "0"
-end
+# if number_1 < 10 && number_2 < 10
+#   puts "1"
+# else
+#   puts "0"
+# end
 
-number_1 = 10
-number_2 = 10
+# number_1 = 10
+# number_2 = 10
 
-if number_1 < 10 && number_2 < 10
-  puts "1"
-else
-  puts "0"
-end
+# if number_1 < 10 && number_2 < 10
+#   puts "1"
+# else
+#   puts "0"
+# end
+
+
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+
+# number = 666
+# if number > 9000
+#   puts "1"
+# elsif number < 9000
+#   puts "-1" 
+# end
+
+# number = 66_600
+# if number > 9000
+#   puts "1"
+# elsif number < 9000
+#   puts "-1" 
+# end
