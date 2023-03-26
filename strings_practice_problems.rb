8bb035eeebd6f7e1d951b0b3f222345b54f1ceda
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-26 15:00:19 -0500

    hardcode #1, working

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -1,2 +1,11 @@
 puts 
 puts "==============="
+
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+# first_name = "Jeffrey"
+# last_name = "Dahmer"
+
+# puts first_name + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
