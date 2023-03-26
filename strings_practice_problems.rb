10d4b3b66130a631578d4c0ff228b7ef7d51fc96
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-26 15:06:56 -0500

    hardcode #2, working

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -6,6 +6,12 @@ puts "==============="
 # first_name = "Jeffrey"
 # last_name = "Dahmer"
 
-# puts first_name + " " + last_name
+# puts first_name + " " + last_name "was a really hungy fellow."
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first_name = "John"
+middle_name = "Wayne"
+last_name = "Gacey"
+
+puts "#{first_name} #{middle_name} #{last_name} was good at clownin' around."
