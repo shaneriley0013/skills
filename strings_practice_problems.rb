f02d64b7f9abaf280d7dd0fcbbb5441cd437ea30
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-26 19:04:29 -0500

    delete sum_array file

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -10,8 +10,27 @@ puts "==============="
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = "John"
-middle_name = "Wayne"
-last_name = "Gacey"
+# first_name = "John"
+# middle_name = "Wayne"
+# last_name = "Gacey"
 
-puts "#{first_name} #{middle_name} #{last_name} was good at clownin' around."
+# puts "#{first_name} #{middle_name} #{last_name} was good at clownin' around."
+
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# puts "Please enter a word."
+# word = gets.chomp.downcase
+
+# if word == "marco"
+#   puts "polo"
+# end
+
+word = " "                       ### this is how you loop until you get the right answer!!!!
+until word == "marco"
+  puts "Please enter a word."
+  word = gets.chomp.downcase
+end
+if word == "marco"
+  puts "polo"
+end
