dcd1906c55519cc03ee5c9dd443d902ecbdbac42
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-26 19:24:27 -0500

    complete #5 strings practice

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -8,6 +8,9 @@ puts "==============="
 
 # puts first_name + " " + last_name "was a really hungy fellow."
 
+
+#=============================================================================#
+
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
 # first_name = "John"
@@ -16,6 +19,7 @@ puts "==============="
 
 # puts "#{first_name} #{middle_name} #{last_name} was good at clownin' around."
 
+#=============================================================================#
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
@@ -34,6 +38,7 @@ puts "==============="
 # if word == "marco"
 #   puts "polo"
 # end
+#=============================================================================
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
@@ -43,3 +48,12 @@ puts "==============="
 
 # puts "My favorite colors are " + color_1 + ", " + color_2 + ", " + "and " + color_3 + "."
 
+#=============================================================================#
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# color_1 = "dark-green"
+# color_2 = "black"
+# color_3 = "gold"
+
+# puts "My favorite colors are #{color_1}, #{color_2}, and #{color_3}."
