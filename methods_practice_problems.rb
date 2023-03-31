02671c9a8ddb9828c091633e0d7cf8bc0a846990
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-31 10:46:13 -0500

    complete methods up to #5

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -2,23 +2,79 @@
 
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+# puts "Please enter a word:"
+# word = gets.chomp.upcase
+# puts word
+
+      ### OR
+
+
+# puts "Please enter a word:"
+# word = gets.chomp
+# puts word.upcase
+
+##==========================================================##
+
+
+
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "Please enter a number:"
+# number = gets.chomp.to_i
+# if number > 100
+#   puts "That's a big number!"
+# else
+#   puts "That's not a big number."
+# end
+
+##==========================================================##
+
+
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+# puts "Please enter a number:"
+# number_1 = gets.chomp.to_i
+
+# puts "Please enter another number:"
+# number_2 = gets.chomp.to_i
+
+# sum = number_1 + number_2
+
+# puts "The sum of your two nummbers is #{sum}."
+
+##==========================================================##
+
+
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+puts "Please enter a word:"
+word = gets.chomp
+puts word.reverse
+
+
+##==========================================================##
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+
+
+
+##==========================================================##
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
+##==========================================================##
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
+##==========================================================##
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+##==========================================================##
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+##==========================================================##
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
+##==========================================================##
 
 # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
