9626b31b6b369c68d3ad19d77756a31e3d25d80b
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-01 16:06:56 -0500

    complete methods up to #9

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -101,18 +101,37 @@
 ##==========================================================##
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
-puts "Please enter a word:"
-word_length = gets.chomp.length
-p "The number of letters in your word is: #{word_length}"
-
-
+# puts "Please enter a word:"
+# word_length = gets.chomp.length
+# p "The number of letters in your word is: #{word_length}"
 
 
 ##==========================================================##
+
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# puts "Please enter a number:"
+# number = gets.to_i
+# if number < 0
+#   puts "That's a negative number"
+# end
+
+# puts "Please enter a number:"
+# number = gets.to_i
+# if number < 0
+#   puts "That's a negative number"
+# else
+#   puts "That's not a negitive number"
+# end
+
+
 
 ##==========================================================##
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+
+
+
 
 ##==========================================================##
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
