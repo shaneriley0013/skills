e2922bb4e04ac352e3e8eea5e8d2e875d45e544b
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-02 14:26:54 -0500

    loops2_practice #3 complete

--- loops2_practice_problems.rb
+++ loops2_practice_problems.rb
@@ -40,14 +40,22 @@
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
-puts "Enter a word:"
+# puts "Enter a word:"
+# word = gets.chomp
+# while word != "stop"
+#   puts "Enter a word:"
+#   word = gets.chomp
+# end
 
 
 
 ###=================================================================================###
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+number = 0
+while number < 100
 
+end
 
 
 
