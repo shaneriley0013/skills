2fa54887f1b0f7591726c8657c6e636da4df01fc
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-02 16:44:24 -0500

    complete #2 and #3

--- arrays1_practice_problems.rb
+++ arrays1_practice_problems.rb
@@ -21,7 +21,32 @@
 
 ##==============================================================================####
 
-# 2. Create an words to store 4 letters. Then change the second letter to a number and print the words on one line.
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# letters = ["F", "U", "C", "K"]
+# p letters
+# letters[1] = 0
+# p letters
+
+
+
+##==============================================================================####
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [0, 6, 6, 6, 0]
+i = 0
+while i < numbers.length
+  puts numbers[i]
+  i += 1
+end
+
+
+
+
+##==============================================================================####
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
 
 
@@ -30,25 +55,7 @@
 
 ##==============================================================================####
 
-# 3. Create an words to store 5 numbers. Then print out each number on separate lines with a while loop.
-
-
-
-
-
-
-##==============================================================================####
-
-# 4. Create an words to store 1 number. Then add three more numbers to the words and print the words on one line.
-
-
-
-
-
-
-##==============================================================================####
-
-# 5. Create an words to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the words on one line.
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the words on one line.
 
 
 
