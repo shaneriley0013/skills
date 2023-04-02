5d618f77377d7dfd0454f9388db965b846e9a452
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-02 14:31:21 -0500

    loops2_practice #4 complete

--- loops2_practice_problems.rb
+++ loops2_practice_problems.rb
@@ -53,8 +53,9 @@
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 number = 0
-while number < 100
-
+while number < 101
+  puts number
+  number += 5
 end
 
 
